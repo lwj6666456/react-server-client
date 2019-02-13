688d5138dd881d04ea4e1fd62098406294592288
lwj6666456 <a313908900>
2019-02-13 18:34:37 +0800

    初始化

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const ReactSSR = require('react-dom/server')
+const serverEntry = require('../dist/server-entry').default
+const app = express()
+app.get('*', function (req, res) {
+    const appString = ReactSSR.renderToString(serverEntry)
+    res.send(appString)
+})
+app.listen(3333, function () {
+    console.log("server is listening on 3333")
+})
