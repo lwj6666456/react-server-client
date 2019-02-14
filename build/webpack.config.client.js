4cc28be83e66b9e473d6eda9fe95b356665a5bc1
lwj6666456 <a313908900>
2019-02-14 20:06:26 +0800

    存档

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -4,63 +4,63 @@ const HTMLPlugin = require('html-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 const config = {
-    mode: "production",
-    entry: {
-        app: path.join(__dirname, '../client/app.js')
-    },
-    output: {
-        filename: '[name].[hash].js',
-        path: path.join(__dirname, '../dist'),
-        publicPath: '/public/'
-    },
-    module: {
-        rules: [{
-            enforce: "pre",
-            test: /.(ja|jsx)$/,
-            loader: "eslint-loader",
-            exclude:[
-                path.resolve(__dirname,'../node_modules')
-            ]
-        },{
-            test: /.jsx$/,
-            loader: "babel-loader"
-        }, {
-            test: /.js$/,
-            loader: "babel-loader",
-            exclude: [
-                path.join(__dirname, '../node_modules')
-            ]
-        },]
-    },
-    plugins: [
-        new HTMLPlugin({
-            template: path.join(__dirname, '../client/template.html')
-        })
-    ]
+  mode: "production",
+  entry: {
+    app: path.join(__dirname, '../client/app.js')
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/'
+  },
+  module: {
+    rules: [{
+      enforce: "pre",
+      test: /.(js|jsx)$/,
+      loader: "eslint-loader",
+      exclude: [
+        path.resolve(__dirname, '../node_modules')
+      ]
+    }, {
+      test: /.jsx$/,
+      loader: "babel-loader"
+    }, {
+      test: /.js$/,
+      loader: "babel-loader",
+      exclude: [
+        path.join(__dirname, '../node_modules')
+      ]
+    },]
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, '../client/template.html')
+    })
+  ]
 
 }
 if (isDev) {
-    config.mode='development'
-    config.entry = {
-        app: [
-            'react-hot-loader/patch',
-            path.join(__dirname, '../client/app.js')
-        ]
+  config.mode = 'development'
+  config.entry = {
+    app: [
+      'react-hot-loader/patch',
+      path.join(__dirname, '../client/app.js')
+    ]
+  }
+  config.devServer = {
+    host: '0.0.0.0',
+    port: '8888',
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+    overlay: {
+      errors: true//错误信息 黑框
+    },
+    publicPath: '/public/',
+    historyApiFallback: {
+      index: '/public/index.html'
     }
-    config.devServer = {
-        host: '0.0.0.0',
-        port: '8888',
-        contentBase: path.join(__dirname, '../dist'),
-        hot: true,
-        overlay: {
-            errors: true//错误信息 黑框
-        },
-        publicPath: '/public/',
-        historyApiFallback: {
-            index: '/public/index.html'
-        }
-    }
-    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 module.exports = config
--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -1,39 +1,39 @@
 const path = require('path')
 const isDev = process.env.NODE_ENV === 'development'
-config= {
-    mode: "production",
-    target: "node",
-    entry: {
-        app: path.join(__dirname, '../client/server-entry.js')
-    },
-    output: {
-        filename: 'server-entry.js',
-        path: path.join(__dirname, '../dist'),
-        publicPath: '/public/',
-        libraryTarget: "commonjs2"
-    },
-    module: {
-        rules: [{
-            enforce: "pre",
-            test: /.(ja|jsx)$/,
-            loader: "eslint-loader",
-            exclude:[
-                path.resolve(__dirname,'../node_modules')
-            ]
-        },{
-            test: /.jsx$/,
-            loader: "babel-loader"
-        },{
-            test:/.js$/,
-            loader: "babel-loader",
-            exclude:[
-                path.join(__dirname,'../node_modules')
-            ]
-        },]
-    }
+config = {
+  mode: "production",
+  target: "node",
+  entry: {
+    app: path.join(__dirname, '../client/server-entry.js')
+  },
+  output: {
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/',
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [{
+      enforce: "pre",
+      test: /.(js|jsx)$/,
+      loader: "eslint-loader",
+      exclude: [
+        path.resolve(__dirname, '../node_modules')
+      ]
+    }, {
+      test: /.jsx$/,
+      loader: "babel-loader"
+    }, {
+      test: /.js$/,
+      loader: "babel-loader",
+      exclude: [
+        path.join(__dirname, '../node_modules')
+      ]
+    },]
+  }
 
 }
 if (isDev) {
-    config.mode='development'
+  config.mode = 'development'
 }
 module.exports = config
